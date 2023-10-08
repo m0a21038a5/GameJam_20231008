d6b0cb933b96723bd6d294d4a0bcf67a863a31aa
watanabe <g021g1093@g.neec.ac.jp>
2023-10-08 15:35:05 +0900

    テストSE追加とAudioManagerの作成

---
+++ Assets/SE/Sound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    [SerializeField]
+    private SoundManager soundManager;
+
+
+    // 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            soundManager.Play("Hit");
+        }
+    }
+}
---
+++ Assets/SE/SoundManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class SoundData
+    {
+        public string name;
+        public AudioClip audioClip;
+    }
+
+    [SerializeField]
+    private SoundData[] soundDatas;
+
+    private AudioSource[] audioSourceList = new AudioSource[30];
+
+    // 別名をキーとした管理用Dictionary
+    private Dictionary<string, SoundData> soundDictionary = new Dictionary<string, SoundData>();
+
+    private void Awake()
+    {
+        //auidioSourceList配列の数だけAudioSourceを自分自身に生成して配列に格納
+        for (var i = 0; i < audioSourceList.Length; ++i)
+        {
+            audioSourceList[i] = gameObject.AddComponent<AudioSource>();
+        }
+
+        //soundDictionaryにせっと
+        foreach (var soundData in soundDatas)
+        {
+            soundDictionary.Add(soundData.name, soundData);
+        }
+    }
+
+    //未使用のAudioSourceの取得 全て使用中の場合はnullを返却
+    private AudioSource GetUnusedAudioSource()
+    {
+        for (var i = 0; i < audioSourceList.Length; ++i)
+        {
+            if (audioSourceList[i].isPlaying == false) return audioSourceList[i];
+        }
+
+        return null; //未使用のAudioSourceは見つかりませんでした
+    }
+
+
+    //指定された別名で登録されたAudioClipを再生
+    public void Play(string name)
+    {
+        if (soundDictionary.TryGetValue(name, out var soundData)) //管理用Dictionary から、別名で探索
+        {
+            Play(soundData.audioClip); //見つかったら、再生
+        }
+        else
+        {
+            Debug.LogWarning($"その別名は登録されていません:{name}");
+        }
+    }
+
+    //指定されたAudioClipを未使用のAudioSourceで再生
+    public void Play(AudioClip clip)
+    {
+        var audioSource = GetUnusedAudioSource();
+        if (audioSource == null) return;
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
